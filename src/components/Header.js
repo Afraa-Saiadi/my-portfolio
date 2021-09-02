0c45af0c56a3347f3eb76d77d459517902c8e0d2
Afraa-Saiadi <emsham1@outlook.com>
2021-09-02 13:48:56 +0200

    deploy my portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,18 @@ import React from 'react';
 import { SocialIcon } from 'react-social-icons';
 import { Link  } from 'react-scroll';
 import  background from '../rotterdam.jpg';
-import { init } from 'ityped';
+// import { init } from 'ityped';
 import {useEffect , useRef} from 'react';
 
 
 export default function Header() {
-    const textRef = useRef();
-    useEffect(() => {
-        init(textRef.current,{ 
-            showCursor: false,
-            backSpeed:  60,
-            strings: [' Web Developer','Welcome to my portfolio' ] })
-    }, [])
+    // const textRef = useRef();
+    // useEffect(() => {
+    //     init(textRef.current,{ 
+    //         showCursor: false,
+    //         backSpeed:  60,
+    //         strings: [' Web Developer','Welcome to my portfolio' ] })
+    // }, [])
     return (
         <div id='header' style={{height:'700px',  backgroundImage:`url(${background})`,backgroundSize:'100% 100%'}} >
             
@@ -31,7 +31,8 @@ export default function Header() {
                         
                     </div> 
                     <div  style={{textAlign:'center' ,font:'50px Arial sans-serif', color:'white',marginTop:'200px' }}>
-                        Afraa Saiadi<br/><span ref={textRef}><br/></span><br/>
+                        Afraa Saiadi<br/>
+                        {/* <span ref={textRef}><br/></span><br/> */}
                         <a  target='_blank' href ='Afraa Saiadi CV NL.pdf' download>Download cv</a>
                     </div>
                     <div style={{marginTop:'30px' , textAlign:'center'}}>
